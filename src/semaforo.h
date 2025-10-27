cae44bcf5e7d4ab0fefb0abdd8fb8fb35ce7e50d
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-10-27 20:04:58 -0300

    feat: declarar as portas dos LEDs no arquivo semaforo.h

--- src/semaforo.h
+++ src/semaforo.h
@@ -1,2 +1,7 @@
 //Chamando a biblioteca do Arduino.
 #include <Arduino.h>
+
+//Declarando as portas dos LEDs.
+uint32_t portaLEDVermelho = 13;
+uint32_t portaLEDAmarelo = 12;
+uint32_t portaLEDVerde = 11;

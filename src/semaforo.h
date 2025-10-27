b1caa59cafcf7b765e9afd3fabb654b5c73c7921
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-10-27 20:18:11 -0300

    feat: adicionar funções para acender e trocar os leds e ligar o semáforo no arquivo semaforo.h

--- src/semaforo.h
+++ src/semaforo.h
@@ -20,10 +20,77 @@ void setup() {
   pinMode(portaLEDAmarelo, OUTPUT);
   pinMode(portaLEDVerde, OUTPUT);
   
-  Serial.begin(115000);
+  Serial.begin(115200);
+}
+
+//Função para ligar o LED.
+void ligarLED(int corDoLED){
+  bool taLigado = true;
+  switch(corDoLED){
+    case 0:
+    	digitalWrite(portaLEDVermelho, taLigado);
+    	digitalWrite(portaLEDAmarelo, !taLigado);
+    	digitalWrite(portaLEDVerde, !taLigado);
+    	break;
+    case 1:
+    	digitalWrite(portaLEDVermelho, !taLigado);
+    	digitalWrite(portaLEDAmarelo, taLigado);
+    	digitalWrite(portaLEDVerde, !taLigado);
+    	break;
+    case 2:
+    	digitalWrite(portaLEDVermelho, !taLigado);
+    	digitalWrite(portaLEDAmarelo, !taLigado);
+    	digitalWrite(portaLEDVerde, taLigado);
+    	break;
+    default:
+    	Serial.println("Meu caro dev, a porta do pino inserida não existe.");
+  }
+}
+
+//Função para trocar o LED que está aceso dado o tempo de cada um.
+void trocarLED(){
+  led++;
+  tempoAnterior = tempoAtual;
+  
+  if(led > 2){
+    led = 0;
+  }
+}
+
+//Função para ligar o semáforo com base nos tempos definidos para cada led.
+void ligarSemaforo(){
+   //Chamando a função millis().
+  tempoAtual = millis();
+  tempoQuePassou = tempoAtual - tempoAnterior;
+  
+  //LED vermelho ligado.
+  if(led == 0){
+    ligarLED(0);
+    if(tempoQuePassou >=6000){
+      trocarLED();
+    }
+  }
+  
+  //LED amarelo ligado.
+  else if(led == 1){
+    ligarLED(1);
+    if(tempoQuePassou >=4000){
+      trocarLED();
+    }
+  }
+  
+  //LED verde ligado.
+  else if(led == 2){
+    ligarLED(2);
+    if(tempoQuePassou >=2000){
+      trocarLED();
+    }
+  }
 }
 
 //Função para manter a lógica do semáforo em constante repetição.
 void loop() {
-
+	
 }
+
+

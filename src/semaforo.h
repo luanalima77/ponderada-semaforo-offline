ac7caf12e06a0cf01662e023b53c0981af9d5cd8
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-10-27 20:06:20 -0300

    feat: adicionar variáveis de tempo no arquivo semaforo.h

--- src/semaforo.h
+++ src/semaforo.h
@@ -5,3 +5,8 @@
 uint32_t portaLEDVermelho = 13;
 uint32_t portaLEDAmarelo = 12;
 uint32_t portaLEDVerde = 11;
+
+//Declarando as variáveis de tempo.
+unsigned long tempoAtual;
+unsigned long tempoAnterior = 0;
+unsigned long tempoQuePassou;
